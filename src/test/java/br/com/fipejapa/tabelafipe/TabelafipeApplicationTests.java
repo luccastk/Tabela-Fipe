3305ac1b7cc25f6b93a95b309fc2994ed28ac4a0
luccastk <teruyuki710@gmail.com>
2024-11-05 21:07:56 -0300

    Init Project

---
+++ src/test/java/br/com/fipejapa/tabelafipe/TabelafipeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fipejapa.tabelafipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabelafipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
